d38a553e9e22cdb64c41492e79a8970e68ff522a
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 09:22:28 -0400

    Setting up HTML Fields

--- question.php
+++ question.php
@@ -1,1 +1,24 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang "en">
+<head>
+    <meta charset ="UTF-8">
+    <title>Question Form</title>
+</head>
+<body>
+    <h1>Questions?</h1>
+    <form method = "post">
+        Question Name:
+        <input type ="text" name = "qname" placeholder = "Question Name"><br><br>
+        Question Body:
+        <textarea name = "body"  ></textarea><br><br>
+        Question Skills:
+        <input type = "text" name = "skills" placeholder = "Question Skills"><br><br>
+        <input type = "submit" name = "submit" value ="Question">
+    </form>
+</body>
+
+</html>
