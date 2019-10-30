dd8609b2e53614576560a4725da5d6482b0888ea
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 09:30:44 -0400

    PHP Validation & Imbedded PHP in HTML

--- question.php
+++ question.php
@@ -4,6 +4,27 @@
         $qname = $_POST['qname'];
         $body = $_POST['body'];
         $skills = $_POST['skills'];
+
+        if($qname == '')
+        {
+            $error = "Question name is required";
+        }
+        else if(strlen($qname)<3)
+        {
+            $error = "Question name character length minimum is 3.";
+        }
+        else if($body == '')
+        {
+            $error = "Question Body is required to be filled.";
+        }
+        else if(strlen($body)>500)
+        {
+            $error = "Question body must contain less than 500 characters";
+        }
+        else
+        {
+            $array = explode(",", $skills);
+        }
     }
 ?>
 
@@ -16,6 +37,18 @@
 <body>
     <h1>Questions?</h1>
     <form method = "post">
+        <?php
+        if(isset($error))
+        {
+            echo $error."<br><br>";
+        }
+        if(isset($array))
+        {
+            echo "Skills: ";
+            print_r($array);
+            echo "<br><br>";
+        }
+        ?>
         Question Name:
         <input type ="text" name = "qname" placeholder = "Question Name"><br><br>
         Question Body:
